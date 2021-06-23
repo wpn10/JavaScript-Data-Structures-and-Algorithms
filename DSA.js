e515a0d6125d3c958a21bcc800dde55e168117f2
Paritosh Wankhade <59663483+wpn10@users.noreply.github.com>
2021-06-24 01:23:10 +0530

    Update DSA.js

--- DSA.js
+++ DSA.js
@@ -137,6 +137,109 @@ class Node{
         this.right = right;
     }
 }
+class BST{
+    constructor(){
+        this.root = null;
+    }
+    add(data) {
+        const node = this.root;
+        if(node==null){
+            this.root = new Node(data);
+            return;
+        }
+        else{
+            const searchtree = function(node){
+                if(data<node.data){
+                    if(node.left==null){
+                        node.left = new Node(data);
+                        return;
+                    }
+                    else return(searchtree(node.left))
+                }
+                else if(data>node.data){
+                    if(node.right==null){
+                        node.right = new Node(data);
+                        return;
+                    }
+                    else return(searchtree(node.right))
+                }
+                else{
+                    return null;
+                }
+        };
+        return searchtree(node);
+     }
+    }
+    findmin(){
+        let current = this.root;
+        while(current.left!==null){
+            current = current.left;
+        }
+        return current.data;
+    }
+    findmax(){
+        let current = this.root;
+        while(current.right!==null){
+            current = current.right;
+        }
+        return current.data;
+    }
+    ispresent(data){
+        let current = this.root;
+        while(current){
+            if(data == current.data){
+                return true;
+            }
+            if(data<current.data){
+                current = current.left;
+            }
+            if(data>current.data){
+                current =current.right;
+            }
+        }
+        return false;
+    }
+    remove(data){
+        const removenode(node,data){
+            if(node==null){
+                return null;
+            }
+            if(data==node.data){
+                if(node.left==null&&node.right==null){
+                    return null;
+                }
+                if(node.left==null){
+                    return node.right;
+                }
+                if(node.right==null){
+                    return node.left;
+                }
+                var temp = node.right;
+                while(temp.left!==null){
+                    temp = temp.left;
+                }
+                node.data = temp.data;
+                node.right = removenode(node.right,temp.data);
+                return node;
+            }
+            else if(data<node.data){
+                node.left = removenode(node.left, data);
+                return node;
+            }
+            else {
+                node.right = removenode(node.right, data);
+                return node;
+            }
+        }
+        this.root = this.removenode(this.root,data);
+    }
+}
+var bst = new BST();
+bst.add(3);bst.add(6);bst.add(1);bst.add(45);bst.add(-23);
+console.log(bst,minh());
+console.log(bst, maxh());
+console.log(bst, isb());
+
 
 
 
